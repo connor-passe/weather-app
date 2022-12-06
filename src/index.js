fd68787a597a88e34f3fcfecd8116139128788e2
Connor Passe <connor.r.passe@gmail.com>
2022-12-06 14:01:13 -0500

    Build form and connect to API hit functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
+/* eslint-disable prefer-destructuring */
 const apiKey = '2356d9f05aa16f6fb4168d86141e10dd';
 
 // Get Coordinates from Location Name
 async function hitGetCoordinates(input) {
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=${apiKey}`);
     const coordinateData = await response.json();
-    console.log(coordinateData);
+    return coordinateData[0];
 }
 
-// Get current weather
-async function hitGetWeather(lat, long) {
+// Get All Weather Data
+async function hitGetWeather(coordinates) {
+    const lat = coordinates.lat;
+    const long = coordinates.lon;
     const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&appid=${apiKey}`);
     const weatherData = await response.json();
+    return weatherData;
+}
+
+// Data pipe from form to functions
+async function handleForm(event) {
+    event.preventDefault();
+    const searchTerm = document.getElementById('searchBar').value;
+    const coordinates = await hitGetCoordinates(searchTerm);
+    const weatherData = await hitGetWeather(coordinates);
     console.log(weatherData);
 }
 
-// hitGetCoordinates('Wabasha, Minnesota');
-hitGetWeather(44.3755, 92.0236);
+const form = document.getElementById('searchForm');
+form.addEventListener('submit', handleForm);
